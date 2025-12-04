538c67871ddb91296c3d5de36652f68cb63d9466
Vaifav SV <vaifav06ai@gmail.com>
2025-12-04 13:57:53 +0530

    Python fuctions - added def vs lambda comparition and interview question

--- index.py
+++ index.py
@@ -1,12 +1,43 @@
-import copy
+def greet(name):
+    print(f"Hello {name}")
 
-a = [1,2,[3,4]]
-b = a.copy()
-c = copy.deepcopy(a)
 
-b[2].append(10)
-c[2].append(20)
+greet("John")
 
-print(a)
-print(b)
-print(c)
+num = 10
+double = lambda x: x * 2
+print(double(num))
+
+
+def logs(level, *msgs, **userDetails):
+    print(f"level - {level}, msgs - {msgs}, user details - {userDetails}")
+
+
+logs(10, "start", "continuing", name="John", age=30, gender="male")
+
+
+def logIn(func):
+    def wrapper(*args, **kwargs):
+        print(f"user logs in...")
+        func(*args, **kwargs)
+
+    return wrapper
+
+
+def auth(func):
+    def wrapper(*args, **kwargs):
+        print(f"user auth ...")
+        func(*args, **kwargs)
+
+    return wrapper
+
+
+@logIn
+@auth
+def user(name):
+    print(name)
+
+
+user("Mark")
+
+print(sum.__doc__)
