113e5a53e9b985c0b3712f3a053392a28d0c9547
Vaifav SV <vaifav06ai@gmail.com>
2025-12-11 14:18:23 +0530

    Path module in python

--- index.py
+++ index.py
@@ -1,24 +1,3 @@
-from functools import reduce
-from operator import *
-from itertools import zip_longest
+from pathlib import Path
 
-nums = [1, 2, 3]
-res = map(lambda x: x * 2, nums)
-print(list(res))
-
-nums = [1, 2, 3, 4, 5]
-evens = filter(lambda x: x % 2 == 0, nums)
-print(list(evens))
-
-nums = [1, 2, 3, 4]
-prod = reduce(lambda a, b: a * b, nums)
-opProd = reduce(mul, nums)
-print(prod)
-print(opProd)
-
-cordinates = ["x", "y", "z", "xy", "xz", "yx", "yz", "zx", "zy"]
-cordinateValues = [1, 2, 3]
-zippedValues = zip(cordinates, cordinateValues)
-zippedLongestValues = zip_longest(cordinates, cordinateValues, fillvalue=0)
-print(list(zippedValues))
-print(list(zippedLongestValues))
+BASE = Path(__file__).resolve(strict=True).parent
