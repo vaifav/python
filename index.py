9db1d1f01461dccd83c4d1b11c6cfd580c6d18b9
Vaifav SV <vaifav06ai@gmail.com>
2025-12-12 21:37:05 +0530

    OS module in python

--- index.py
+++ index.py
@@ -1,3 +1,9 @@
+import os
 from pathlib import Path
 
-BASE = Path(__file__).resolve(strict=True).parent
+BASE = Path(__file__).parent.resolve()
+for root, dir, file in os.walk(BASE):
+    if {".venv"} in dir:
+        r_dir = dir[dir]
+        print(type(r_dir))
+        dir.remove(str(r_dir))
